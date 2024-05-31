aa6b49273f70435e4ed6d9c0bca624cf837a200f
AminSallah <124622454+AminSallah@users.noreply.github.com>
2024-06-01 00:23:36 +0300

    Add files via upload

--- src/Views/PluginSettings.xaml.cs
+++ src/Views/PluginSettings.xaml.cs
@@ -18,25 +18,6 @@ namespace Flow.Launcher.Plugin.Azan.Views
             _viewModel = viewModel;
             this.Settings = settings;
             DataContext = viewModel;
-            // foreach (ComboBoxItem item in SyncComboBox.Items)
-            // {
-            //     if (item.Content.ToString() == Settings.Sync)
-            //     {
-            //         SyncComboBox.SelectedItem = item;
-            //         break;
-            //     }
-            // }
-
-        }
-
-        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            if (e.AddedItems.Count > 0)
-            {
-                Settings.Sync = SyncComboBox.SelectedItem as string;
-                // MessageBox.Show($"Selected item changed to: {selectedValue}");
-                // Call your method or logic here
-            }
         }
 
     }
